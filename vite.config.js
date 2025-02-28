0dcf2c9500a9d675c21f300cc2ad54618395c1ec
boyina-sruthi <125252490+boyina-sruthi@users.noreply.github.com>
2025-03-01 00:41:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Travel-planner-frontend/', // Ensure this matches your GitHub repository name
   plugins: [react()],
   resolve: {
     alias: {
